13b58f038bc17b064d79717c2161cba48cd5e9f9
amana-nminustwo <amana.nminustwo@gmail.com>
2025-11-27 18:22:04 +0530

    eighty percentage design implementation of homescreen at 27/11/25 at 6.21 pm

--- lib/src/presentation/controller/AppstartupController/app_startup_controller.dart
+++ lib/src/presentation/controller/AppstartupController/app_startup_controller.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-
 import 'package:election_management/src/presentation/screens/Auth/loginpage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
